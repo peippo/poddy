6f2c00b9699a2bbb891463f0a6f11207e6729c9c
Janne Peippo <peippo@gmail.com>
2022-04-11 21:15:07 +0300

    Add player close button & transitions

--- src/lib/components/player.svelte
+++ src/lib/components/player.svelte
@@ -12,14 +12,20 @@
 </script>
 
 <script lang="ts">
+	import { fade, fly } from 'svelte/transition';
+
 	export let episode;
 
-	$: status = null;
+	$: status = 'Loading';
 	$: playing = status === 'Playing';
 </script>
 
-<div class="background">
-	<section class="player">
+<div class="background" in:fade out:fade>
+	<section
+		class="player"
+		in:fly={{ y: 20, duration: 500, delay: 300 }}
+		out:fly={{ y: 20, duration: 500 }}
+	>
 		<h2>
 			{#if status}
 				<span class:playing class="status-tag">{status}</span>
@@ -37,6 +43,15 @@
 			Your browser does not support the
 			<code>audio</code> element.
 		</audio>
+
+		<button class="close-button" on:click={() => ($activeEpisode = null)}>
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+				<path
+					fill="currentColor"
+					d="M0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256zM175 208.1L222.1 255.1L175 303C165.7 312.4 165.7 327.6 175 336.1C184.4 346.3 199.6 346.3 208.1 336.1L255.1 289.9L303 336.1C312.4 346.3 327.6 346.3 336.1 336.1C346.3 327.6 346.3 312.4 336.1 303L289.9 255.1L336.1 208.1C346.3 199.6 346.3 184.4 336.1 175C327.6 165.7 312.4 165.7 303 175L255.1 222.1L208.1 175C199.6 165.7 184.4 165.7 175 175C165.7 184.4 165.7 199.6 175 208.1V208.1z"
+				/>
+			</svg>
+		</button>
 	</section>
 </div>
 
@@ -112,5 +127,26 @@
 
 	audio {
 		width: 100%;
+	}
+
+	.close-button {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		position: absolute;
+		top: -12px;
+		right: 0;
+		width: 42px;
+		height: 42px;
+		background: var(--color-black);
+		border-radius: 1000px;
+		border: 0;
+		color: var(--color-pink);
+		opacity: 0.9;
+
+		&:hover {
+			cursor: pointer;
+			opacity: 1;
+		}
 	}
 </style>
