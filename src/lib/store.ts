1ca64da7162827f5d7b12be7b29f6d19e54d30b5
Janne Peippo <peippo@gmail.com>
2022-04-17 10:45:34 +0300

    Keep settings in local storage

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,7 +1,8 @@
 import { writable } from 'svelte/store';
+import { persist, localStorage } from '@macfja/svelte-persistent-store';
 
 export const selectedPodcastId = writable(null);
 export const activeEpisode = writable(null);
 export const isPlaying = writable(false);
-export const selectedCategories = writable([]);
-export const selectedLanguages = writable(['en']);
+export const selectedCategories = persist(writable([]), localStorage(), 'selectedCategories');
+export const selectedLanguages = persist(writable(['en']), localStorage(), 'selectedLanguages');
